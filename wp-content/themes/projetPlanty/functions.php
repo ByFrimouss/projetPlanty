6d835579356a3e2d648ec6852236ca3c642ba02f
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2024-08-07 08:58:36 +0400

    Personnalisation des boutons et mise en place du hook

--- wp-content/themes/projetPlanty/functions.php
+++ wp-content/themes/projetPlanty/functions.php
@@ -9,7 +9,14 @@ wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 //POUR RELIER LE CSS DU THÈME ENFANT //
 wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-
 }
 
+// METTRE EN PLACE LE HOOK DU LIEN ADMIN //
+function ajout_lien_admin($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $items .= '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajout_lien_admin', 10, 2);
 ?>
